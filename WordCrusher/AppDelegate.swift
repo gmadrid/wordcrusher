1c70e45e477d4052826ac1de8e282ea0d5c89094
George Madrid <gmadrid@gmail.com>
2017-06-20 11:35:14 -0400

    Initial Commit

---
+++ WordCrusher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WordCrusher
+//
+//  Created by George Madrid on 6/20/17.
+//  Copyright © 2017 George Madrid. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ WordCrusher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WordCrusher
+//
+//  Created by George Madrid on 6/20/17.
+//  Copyright © 2017 George Madrid. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
