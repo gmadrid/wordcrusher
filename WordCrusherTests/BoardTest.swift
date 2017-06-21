1dfb1582a009371bba862f89f4926bbd8bafe828
George Madrid <gmadrid@gmail.com>
2017-06-21 13:56:38 -0400

    swift format. Mostly spacing changes.

--- WordCrusherTests/BoardTest.swift
+++ WordCrusherTests/BoardTest.swift
@@ -11,51 +11,51 @@ import XCTest
 
 class BoardTest: XCTestCase {
   var board: Board!
-  
+
   override func setUp() {
     super.setUp()
     // Put setup code here. This method is called before the invocation of each test method in the class.
   }
-  
+
   override func tearDown() {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     super.tearDown()
   }
-  
+
   private func adjacentString(to index: Board.CellIndex) throws -> String {
     let adjacent =
       try board.adjacent(to: index)
-        .map { try board.lookup(index: $0) }
-        .sorted()
+      .map { try board.lookup(index: $0) }
+      .sorted()
     return String(adjacent)
   }
-  
+
   private func trieWithWords(_ words: [String]) -> Trie {
     let trie = Trie()
     words.forEach { trie.insert(word: $0) }
     return trie
   }
-  
+
   func testNoReuseCells() throws {
     let trie = trieWithWords(["hele", "zxwy"])
-    
+
     // The 2 x 2 board looks like this:
     //
     //   H   L
     // Q   E
     //   X   Y
     // Z   W
-    // 
+    //
     board = try Board(rows: 2, cols: 4, contents: "QHELZXWY")
-    
+
     // "hele" cannot be found without reusing the 'e'.
     let list = board.searchAll(in: trie)
     XCTAssertEqual(["zxwy"], list.sorted())
   }
-  
+
   func testSearchMaxDepth() throws {
     let trie = trieWithWords(["hello", "hell", "bell", "belly"])
-    
+
     // The 3x4 board looks like this:
     //
     //   X   V
@@ -66,19 +66,19 @@ class BoardTest: XCTestCase {
     // U   R
     //
     board = try Board(rows: 3, cols: 4, contents: "HXOVBELLUQRY")
-    
+
     let list = board.searchAll(in: trie)
-    XCTAssertEqual([ "bell", "belly", "hell", "hello" ], list.sorted())
-    
+    XCTAssertEqual(["bell", "belly", "hell", "hello"], list.sorted())
+
     let shortList: [String] = board.searchAll(in: trie, maxDepth: 4)
     XCTAssertEqual(2, shortList.count)
   }
-  
+
   func testSearch() throws {
     let trie = trieWithWords(["hello", "kitty", "hell"])
-    
+
     // The 3x4 board looks like this:
-    //    
+    //
     //     I   T
     //   K   T
     //     H   H
@@ -87,7 +87,7 @@ class BoardTest: XCTestCase {
     //   L   O
     //
     board = try Board(rows: 3, cols: 4, contents: "KITTEHSHLLOY")
-    
+
     // Test that it finds all versions of word and that it will find a word that continues another.
     let list = board.search(from: (1, 1), in: trie)
     XCTAssertEqual(["hell", "hell", "hello"], list.sorted())
@@ -95,12 +95,12 @@ class BoardTest: XCTestCase {
     // Test that it will stop when nothing matches.
     let list2 = board.search(from: (1, 3), in: trie)
     XCTAssertEqual([], list2)
-    
+
     // Test that it will stop when a word almost matches.
     let list3 = board.search(from: (0, 0), in: trie)
     XCTAssertEqual([], list3)
   }
-  
+
   func testSearchAll() throws {
     let trie = trieWithWords(["hello", "kitty", "hell"])
 
@@ -114,12 +114,11 @@ class BoardTest: XCTestCase {
     //   L   H
     //
     board = try Board(rows: 3, cols: 4, contents: "KITTOHEYLLHX")
-    
+
     let list = board.searchAll(in: trie)
     XCTAssertEqual(["hell", "hell", "hello", "hello", "kitty"], list.sorted())
-
   }
-  
+
   func testBoard() throws {
     // The 3x4 board looks like this:
     //
@@ -133,19 +132,18 @@ class BoardTest: XCTestCase {
     board = try Board(rows: 3, cols: 4, contents: "ABCDEFGHIJKL")
 
     // This little exercise tests both adjacent and lookup.
-    
+
     // Try some even columns
     XCTAssertEqual("afij", try adjacentString(to: (1, 0)))
     XCTAssertEqual("cfhjkl", try adjacentString(to: (1, 2)))
     XCTAssertEqual("bef", try adjacentString(to: (0, 0)))
     XCTAssertEqual("ej", try adjacentString(to: (2, 0)))
     XCTAssertEqual("gjl", try adjacentString(to: (2, 2)))
-    
+
     // Try some odd columns
     XCTAssertEqual("acf", try adjacentString(to: (0, 1)))
     XCTAssertEqual("ch", try adjacentString(to: (0, 3)))
     XCTAssertEqual("abcegj", try adjacentString(to: (1, 1)))
     XCTAssertEqual("ghk", try adjacentString(to: (2, 3)))
   }
-  
 }
--- WordCrusherTests/TrieTest.swift
+++ WordCrusherTests/TrieTest.swift
@@ -11,7 +11,7 @@ import XCTest
 
 class TrieTest: XCTestCase {
   var trie: Trie!
-  
+
   override func setUp() {
     super.setUp()
 
@@ -21,17 +21,17 @@ class TrieTest: XCTestCase {
     trie.insert(word: "dogfood")
     trie.insert(word: "doggerel")
   }
-  
+
   override func tearDown() {
     super.tearDown()
   }
-  
+
   func testInsertionAndContains() {
     XCTAssert(trie.contains(word: "dog"))
     XCTAssert(trie.contains(word: "car"))
     XCTAssert(trie.contains(word: "dogfood"))
     XCTAssert(trie.contains(word: "doggerel"))
-    
+
     XCTAssert(!trie.contains(word: "cat"))
     XCTAssert(!trie.contains(word: "do"))
     XCTAssert(!trie.contains(word: "dogg"))
@@ -39,28 +39,28 @@ class TrieTest: XCTestCase {
     XCTAssert(!trie.contains(word: "dof"))
     XCTAssert(!trie.contains(word: "dogfoodxx"))
     XCTAssert(!trie.contains(word: "doggerelxx"))
-    
+
     trie.insert(word: "dogg")
     XCTAssert(trie.contains(word: "dogg"))
   }
-  
-  enum TestErr : Error {
+
+  enum TestErr: Error {
     case UnexpectedNilTrieToken
   }
-  
+
   func expectToken(_ token: TrieToken?) throws -> TrieToken {
     guard let token = token else {
       throw TestErr.UnexpectedNilTrieToken
     }
     return token
   }
-  
+
   func testSearch() throws {
-    let search : TrieToken = trie.search()
-    
+    let search: TrieToken = trie.search()
+
     // Test nothing at start of search.
     XCTAssertNil(search.next("a"))
-    
+
     // Test successful searches both in middle and at end of word.
     var token = try expectToken(search.next("d"))
     XCTAssert(!token.isWord)
@@ -68,10 +68,10 @@ class TrieTest: XCTestCase {
     XCTAssert(!token.isWord)
     token = try expectToken(token.next("g"))
     XCTAssert(token.isWord)
-    
+
     // Test bad search in middle.
     XCTAssertNil(token.next("x"))
-    
+
     // Test successful searches continuing from end of word.
     token = try expectToken(token.next("f"))
     XCTAssert(!token.isWord)
@@ -81,7 +81,7 @@ class TrieTest: XCTestCase {
     XCTAssert(!token.isWord)
     token = try expectToken(token.next("d"))
     XCTAssert(token.isWord)
-    
+
     XCTAssertNil(token.next("f"))
   }
 }
